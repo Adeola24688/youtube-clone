93abacf7bb22feb85b00731495e2acef6272f9cb
Shefiu Abdullahi <abdullahishefiu99@gmail.com>
2026-04-16 10:32:16 +0100

    feat: integrate video data fetching and display in PlayVideo component

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -6,16 +6,38 @@ import share from '../../assets/share.png'
 import save from '../../assets/save.png'
 import jack from '../../assets/jack.png'
 import user_profile from '../../assets/user_profile.jpg'
+import { useState } from 'react'
+import { value_converter } from '../../data'
+import moment from 'moment'
 
 
-const PlayVideo = () => {
+const PlayVideo = ({ videoId }) => {
+
+       const [apiData, setApiData] = useState(null);
+
+       const fetchVideoData = async () => {
+
+        const videoDetails_Url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`;
+        await fetch(videoDetails_Url).then(res => res.json()).then(data => setApiData(data.items[0]));
+
+
+         }
+
+         useEffect(() => {
+            fetchVideoData();
+            }, [])
+
+
+
+
+
     return (
         <div className='play-video'>
-            <video src={video1} controls autoPlay muted></video>
-            <h3>Best channel to learn coding that help you to be a web developer
-            </h3>
+             <iframe  src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <h3>{apiData ? apiData.snippet.title : "Title Here"} </h3>
+            
             <div className='play-video-info'>
-                <p>1525 Views &bull; 2 days ago </p>
+                <p>{apiData?value_converter(apiData.statistics.viewCount) : "16k"} Views &bull; {moment(apiData.snippet.publishedAt).fromNow()} </p>
                 <div>
                     <span><img src={like} alt="" /> 125</span>
                     <span><img src={dislike} alt="" /> 2</span>
